5deb9f0a3301f21cbf0f8b5afd3de0f009a2029d
Carlos Diones <carlosdiones28@gmail.com>
2022-02-18 21:24:34 -0300

    html and css incomplete

---
+++ interactive/js01/script.js
@@ -0,0 +1,21 @@
+// #container, #item
+
+var container = document.getElementById("container");
+var item = document.getElementById("item");
+var direction = "r";
+
+function moveLeft(){
+   
+}
+
+function moveRight(){
+
+}
+
+function moveUp(){
+
+}
+
+function moveDown(){
+
+}
