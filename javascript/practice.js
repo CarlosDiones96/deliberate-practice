a7a0013f26b538579d7e4472be83ea18ba121d36
Carlos Diones <carlosdiones28@gmail.com>
2022-02-28 23:19:14 -0300

    more examples

--- javascript/practice.js
+++ javascript/practice.js
@@ -83,3 +83,86 @@ function trace(o, m){
 
 
 //====================================================
+
+window.location = "https://www.example.com";
+setTimeout(function(){alert("hello")}, 2000);
+
+var timestamp = document.getElementById("timestamp");
+
+if(timestamp.firstChild == null){
+    timestamp.appendChild(
+        document.createTextNode(new Date().toString())
+    );
+}
+
+timestamp.style.backgroundColor = "yellow";
+timestamp.className = "highlight";
+
+timestamp.onclick = function() {this.innerHTML = new Date().toString();}
+
+
+//Simple client-side javascript for revealing content
+window.onload = function(){
+    var elements = document.getElementsByClassName("reveal");
+    for(var i = 0; i < elements.length; i++){
+        var elt = elements[i];
+        var title = elt.getElementsByClassName("handle")[0];
+        title.onclick = function(){
+            if(elt.className == "reveal"){
+                elt.className = "revealed";
+            }else if(elt.className == "revealed"){
+                elt.className = "reveal";
+            }
+        };
+    }
+};
+
+//Define a function to display the current time
+function displayTime(){
+    var elt = document.getElementById("clock");
+    var now = new Date();
+    elt.innerHTML = now.toLocaleTimeString();
+    setTimeout(displayTime, 1000);
+}
+window.onload = displayTime;
+
+
+//====================================================
+
+var e = "", r = "";
+do{
+    e = prompt("Expression: " + e + "\n" + r + "\n", e);
+    try{
+        r = "Result: " + eval(e);
+    }catch(ex){
+        r = ex;
+    }
+
+}while(e);
+void 0;
+
+//====================================================
+
+//Generating document content at load time
+function factorial(n){
+    if(n <= 1) return n;
+    else return n*factorial(n-1);
+}
+
+document.write("<table>");
+document.write("<tr><th>n</th><th>n!</th></tr>");
+for(var i = 1; i <= 10; i++){
+    document.write("<tr><td>" + i + "</td><td>" + factorial(i) + "</td></tr>");
+}
+document.write("</table>");
+document.write("Generated at: " + new Date());
+
+//Asynchronously loading and executing a script
+function loadasync(url){
+    var head = document.getElementsByTagName("head")[0];
+    var s = document.createElement("script");
+    s.src = url;
+    head.appendChild(s);
+}
+
+//====================================================
