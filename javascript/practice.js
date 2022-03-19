72772a8792d8d4f84d027526f72c56f47a7ae79d
Carlos <carlosdiones28@gmail.com>
2022-03-19 18:06:23 -0300

    more js examples

--- javascript/practice.js
+++ javascript/practice.js
@@ -121,3 +121,175 @@ function fadeOut(e, oncomplete, time){
 }
 
 // => Page 454 (Computed Styles)
+var title = document.getElementById("section1title");
+var titlestyles = window.getComputedStyle(element, nulll);
+
+//Querying computed styles and setting inline styles
+function scale(e, factor){
+    var size = parseInt(window.getComputedStyle(e, "").fontSize);
+    e.style.fontSize = factor*size + "px";
+}
+
+function scaleColor(e, factor){
+    var color = window.getComputedStyle(e, "").backgroundColor;
+    var components = color.match(/[\d\.]+/g);
+    for(var i =0; i < 3; i++){
+        var x = Number(components[i]) * factor;
+        x = Math.round(Math.min(Math.max(x, 0), 255));
+        components[i] = String(x);
+    }
+    if(components.length == 3){
+        e.style.backgroundColor = "rgb(" + components.join() + ")";
+    }else{
+        e.style.backgroundColor = "rgba(" + components.join() + ")";
+    }
+}
+
+//
+function grabAttention(e){
+    e.className = "attention";
+}
+
+function releaseAttention(e){
+    e.className = "";
+}
+//
+
+// treat className as set of CSS classes
+function classList(e){
+    if(e.classList){
+        return e.classList;
+    }else{
+        return new CSSClassList(e);
+    }
+}
+
+function CSSClassList(e){
+    this.e = e;
+}
+
+CSSClassList.prototype.contains = function(c){
+    if(c.length === 0 || c.indexOf(" ") != -1){
+        throw new Error("Invalid class name: '" + c + "'");
+    }
+    var classes = this.e.className;
+    if(!classes) return false;
+    if(classes === c) return true;
+
+    return classes.search("\\b"+ c + "\\b") != -1;
+};
+
+CSSClassList.prototype.add = function(c){
+    if(this.contains(c)) return;
+    var classes = this.e.className;
+    if(classes && classes[classes.length-1] != " "){
+        c = " " + c;
+    }
+    this.e.className += c;
+};
+
+CSSClassList.prototype.remove = function(c){
+    if(c.length === 0 || c.indexOf(" ") != -1){
+        throw new Error("Invalid class name : '" + c + "'");
+    }
+    var pattern = new RegExp("\\b" + "\\s*", "g");
+    this.e.className = this.e.className.replace(pattern, "");
+};
+
+CSSClassList.prototype.toggle = function(c){
+    if(this.contains(c)){
+        this.remove(c)
+        return false;
+    }else{
+        this.add(c);
+        return true;
+    }
+};
+
+CSSClassList.ptototype.toString = function(){
+    return this.e.className;
+};
+
+CSSClassList.prototype.toArray = function(){
+    return this.e.className.match(/\b\w+\b/g) || [];
+}
+
+//--------------------
+function disableStylesheets(ss){
+    if(typeof ss === "number"){
+        document.styleSheets[ss].disabled = true;
+    }else{
+        var sheets = document.querySelectorAll(ss);
+        for(var i = 0; i < sheets.length; i++){
+            sheets[i].disabled = true;
+        }
+    }
+}
+
+
+//
+var firstRule = document.styleSheets[0].cssRules[0];
+
+document.styleSheets[0].insertRule("H1 { text-weight: bold; }", 0);
+
+//
+var ss = document.styleSheets[0];
+var rules = ss.cssRules ? ss.cssRules : ss.rules;
+
+for(var i = 0; i < rules; i++){
+    var rule = rules[i];
+    if(!rule.selectorText) continue;
+
+    var selector = rule.selectorText;
+    var ruleText = rule.style.cssText;
+
+    if(selector == "h1"){
+        if(ss.insertRule){
+            ss.insertRule("h2 { "+ ruleText+"}", rules.length);
+        }else if(ss.addRule){
+            ss.addRule("h2", ruleText, rules.length);
+        }
+    }
+
+    if(rule.style.textDecoration){
+        if(ss.deleteRule){
+            ss.deleteRule(i);
+        }else if(ss.removeRule){
+            ss.removeRule(i);
+        }
+        i--;
+    }
+}
+
+//creating a new stylesheet
+function addStyles(styles){
+    var styleElt, styleSheet;
+    if(document.createStyleSheet){
+        styleSheet = document.createStyleSheet();
+    }else{
+        var head = document.getElementsByTagName("head")[0];
+        styleElt = document.createElement("style");
+        head.appendChild(styleElt);
+        styleSheet = document.styleSheets[document.styleSheets.length-1];
+    }
+
+    if(typeof styles === "string"){
+        if(styleElt){
+            styleElt.innerHTNL = styles;
+        }else{
+            styleSheet.cssText = styles;
+        }
+    }else{
+        var i = 0;
+        for(selector in styles){
+            if(styleSheet.insertRule){
+                var rule = selector + "{" + styles[selector] + "}";
+                styleSheet.insertRules(rule, i++);
+            }else{
+                styleSheet.addRule(selector, styles[selector], i++);
+            }
+        }
+    }
+}
+
+// NEXT >>> Page 463 [Handling Events]
