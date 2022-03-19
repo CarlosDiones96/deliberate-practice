da4c9fa64f6abd6f642b77053a12ab13ce29f828
Carlos <carlosdiones28@gmail.com>
2022-03-19 19:55:52 -0300

    more react examples

--- react/app.js
+++ react/app.js
@@ -89,5 +89,101 @@ class Square extends React.Component{
     }
 }
 
-// NEXT:  React Tutorial ["Completing the Game"]
+///NEXT:  React Tutorial ["Completing the Game"]
 //https://reactjs.org/tutorial/tutorial.html 
+class Board extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+        };
+    }
+
+    renderSquare(i){
+        return (<Square value={
+            this.state.squares[i]
+        }
+        onClick={
+            ()=> this.handleClick(i)
+        } />
+        );
+    }
+}
+
+//
+class Square extends React.Component{
+    render(){
+        return(
+            <button>
+                className="sqaure"
+                onClick={()=> this.props.onClick()}
+            </button>
+            {this.props.value}
+            );
+    }
+}
+
+class Board extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+        };
+    }
+
+    handleClick(i){
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares: squares});
+    }
+
+    renderSquare(i){
+        return(
+        <Square 
+            value={this.state.squares[i]}
+            onClick={()=> this.handleClick(i)}
+        />
+      );
+    }
+
+    render(){
+        const status = 'Next player: X';
+
+        return(
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div> 
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        );
+    }
+}
+
+/ /**/
+var player = {score: 1, name: 'Jeff'};
+var newPlayer = Object.assign({}, player, {score: 2});
+
+function Square(props){
+    return(
+        <button className="square" onClick={props.onCLick}>
+            {props.value}
+        </button>
+        );
+}
+
+//**/
+// REATC TUTORIAL >>> TAKING TURNS
+// https://reactjs.org/tutorial/tutorial.html#completing-the-game 
