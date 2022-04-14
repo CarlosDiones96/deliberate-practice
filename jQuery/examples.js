e32098da29d92c73d49f5c1451b4b946341b9bdb
Carlos <carlosdiones28@gmail.com>
2022-04-13 22:21:46 -0300

    more jquery examples

--- jQuery/examples.js
+++ jQuery/examples.js
@@ -43,3 +43,64 @@ firstLi.data("contentDiv").html("new content");
 
 // NEXT >> Utility Methods
 // https://learn.jquery.com/using-jquery-core/utility-methods/ 
+
+$.trim("    lots of extra space");
+
+$.each(["foo", "bar", "baz"], function(idx, val){
+    console.log("element " + idx + " is " + val);
+});
+
+$.each({foo: "bar", baz: "bim"}, function(k, v){
+    console.log(k + " : " + v);
+});
+
+var myArray = [1, 2, 3, 5];
+
+if( $.inArray(4, myArray) !== -1){
+    console.log("found it");
+}
+
+//
+var firstObject = {foo: "bar", a: "b"};
+var secondObject = {foo: "baz"};
+
+var newObject = $.extend(firstObject, secondObject);
+
+console.log(firstObject.foo); //baz
+console.log(newObject.foo); //baz
+
+//
+var myFunction = function() {
+    console.log(this);
+};
+var myObject = {
+    foo: "bar"
+};
+
+myFunction(); // window
+
+var myProxyFunction = $.proxy(myFunction, myObject);
+myProxyFunction(); // myObject
+
+//
+var myObject = {
+    myF: function(){
+        console.log(this);
+    }
+};
+
+$("#foo").click(myObject.myFn);
+$("#foo").click($.proxy(myObject, "myF")); 
+
+//
+$.isArray([]);
+$.isFunction(function(){});
+$.isNumeric(3.14);
+
+$.type(true);
+$.type(3);
+$.type("text");
+$.type(/test/);
+
+// Iterating over jQuery and non-jQuery Objects
+// https://learn.jquery.com/using-jquery-core/iterating/
